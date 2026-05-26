b084cf36999b9395d08147043e54daae8451449f
comar1-ncompass <francisc@n-compass.biz>
2026-05-26 17:58:02 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to SQLite successfully")
+}
+
